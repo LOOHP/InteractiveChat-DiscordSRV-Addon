3983070788344abd5e351e496925ef9b6d727868
LOOHP <jamesloohp@gmail.com>
2021-04-17 12:06:19 +0800

    Move away from hardcoding fonts

--- src/main/java/com/loohp/interactivechatdiscordsrvaddon/graphics/MCFont.java
+++ src/main/java/com/loohp/interactivechatdiscordsrvaddon/graphics/MCFont.java
@@ -4,8 +4,18 @@ import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 import java.awt.geom.AffineTransform;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.bukkit.Bukkit;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import com.loohp.interactivechatdiscordsrvaddon.InteractiveChatDiscordSrvAddon;
 
@@ -14,54 +24,149 @@ import net.md_5.bungee.api.ChatColor;
 public class MCFont {
 	
 	private static boolean working = false;
-	private static Font mcFont;
-	private static Font uniFont;
 	
-	private static double uniFontOffsetX = -1.0 / 16.0;
-	private static double uniFontOffsetY = -3.5 / 16.0;
+	private static Map<String, MCFont> FONTS = new LinkedHashMap<>();
+	private static List<String> ORDER = new ArrayList<>();
 
     static {
     	reloadFonts();
     }
     
     public synchronized static void reloadFonts() {
-    	String mcfontPath = InteractiveChatDiscordSrvAddon.plugin.getDataFolder() + "/assets/font/mcfont.ttf";
-    	String unifontPath = InteractiveChatDiscordSrvAddon.plugin.getDataFolder() + "/assets/font/unifont.ttf";
-    	
         try {
-        	File mcfontFile = new File(mcfontPath);
-            mcFont = Font.createFont(Font.TRUETYPE_FONT, mcfontFile).deriveFont(18F);
-            GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(mcFont);
-            
-            File unifontFile = new File(unifontPath);
-            uniFont = Font.createFont(Font.TRUETYPE_FONT, unifontFile).deriveFont(16F);
-            GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(uniFont);
-            
+        	File fontFolder = new File(InteractiveChatDiscordSrvAddon.plugin.getDataFolder() + "/assets/font");
+        	if (!fontFolder.exists()) {
+        		working = false;
+        		return;
+        	}
+        	File fontDataFile = new File(fontFolder, "font.json");
+        	if (!fontDataFile.exists()) {
+        		working = false;
+        		return;
+        	}
+        	InputStreamReader reader = new InputStreamReader(new FileInputStream(fontDataFile), StandardCharsets.UTF_8);
+        	JSONObject json = (JSONObject) new JSONParser().parse(reader);
+        	reader.close();
+        	
+        	Map<String, MCFont> fonts = new LinkedHashMap<>();
+        	List<String> order = new ArrayList<>();
+        	
+        	int i = 0;
+        	for (Object obj : (JSONArray) json.get("providers")) {
+        		i++;
+        		try {
+	        		JSONObject provider = (JSONObject) obj;
+	        		String identifier = provider.get("identifier").toString();
+	                int type = getFontType(provider.get("type").toString());
+	                File file = new File(fontFolder, provider.get("file").toString());
+	                float size = (float) (double) provider.get("size");
+	                JSONArray offsetArray = (JSONArray) provider.get("offset");
+	                double offsetX = (double) offsetArray.get(0);
+	                double offsetY = (double) offsetArray.get(1);
+	                
+	                Font font = Font.createFont(type, file).deriveFont(size);
+	                GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(font);
+	                
+	                MCFont mcfont = new MCFont(identifier, type, file, size, offsetX, offsetY, font);
+	                fonts.put(identifier, mcfont);
+	                order.add(identifier);
+        		} catch (Throwable e) {
+        			Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "[ICDiscordSrvAddon] Unable to load font provider " + i);
+        			e.printStackTrace();
+        		}
+        	}
+        	
+        	Bukkit.getScheduler().runTask(InteractiveChatDiscordSrvAddon.plugin, () -> {
+        		FONTS = fonts;
+            	ORDER = order;
+        	});
+
             working = true;
-        } catch (Exception e) {
-        	Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "[ICDiscordSrvAddon] Unable to import font from " + mcfontPath);
+        } catch (Throwable e) {
+        	Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "[ICDiscordSrvAddon] Unable to load fonts");
             e.printStackTrace();
+            working = false;
         }
     }
 
-    public static Font getStandardFont() {
-		return mcFont;
-	}
-
-	public static Font getBackupFont() {
-		return uniFont;
+    public static MCFont getMCFont(String id) {
+		return FONTS.get(id);
 	}
 
 	public static Font getFont(String text, float fontSize) {
-        if (mcFont.canDisplayUpTo(text) == -1) {
-            return mcFont.deriveFont(fontSize);
-        } else {
-            return uniFont.deriveFont(fontSize).deriveFont(AffineTransform.getTranslateInstance(uniFontOffsetX * fontSize, uniFontOffsetY * fontSize));
-        }
+		for (int i = 0; i < ORDER.size(); i++) {
+			MCFont mcfont = FONTS.get(ORDER.get(i));
+	        if (mcfont.getFont().canDisplayUpTo(text) == -1 || i >= ORDER.size() - 1) {
+	            Font font = mcfont.getFont().deriveFont(fontSize);
+	            if (mcfont.getOffsetX() != 0 || mcfont.getOffsetY() != 0) {
+	            	return font.deriveFont(AffineTransform.getTranslateInstance(mcfont.getOffsetX() * fontSize, mcfont.getOffsetY() * fontSize));
+	            } else {
+	            	return font;
+	            }
+	        }
+		}
+		return null;
     }
     
     public static boolean isWorking() {
     	return working;
     }
+    
+    private static int getFontType(String name) {
+    	switch (name.toUpperCase()) {
+    	case "TRUETYPE_FONT":
+    		return Font.TRUETYPE_FONT;
+    	case "TYPE1_FONT":
+    		return Font.TYPE1_FONT;
+    	default:
+    		return -1;
+    	}
+    }
+    
+    private String identifier;
+    private int type;
+    private File file;
+    private float size;
+    private double offsetX;
+    private double offsetY;
+    private Font font;
+    
+	public MCFont(String identifier, int type, File file, float size, double offsetX, double offsetY, Font font) {
+		this.identifier = identifier;
+		this.type = type;
+		this.file = file;
+		this.size = size;
+		this.offsetX = offsetX;
+		this.offsetY = offsetY;
+		this.font = font;
+	}
+
+	public String getIdentifier() {
+		return identifier;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public File getFile() {
+		return file;
+	}
+
+	public float getSize() {
+		return size;
+	}
+
+	public double getOffsetX() {
+		return offsetX;
+	}
+
+	public double getOffsetY() {
+		return offsetY;
+	}
+
+	public Font getFont() {
+		return font;
+	}
     
 }
