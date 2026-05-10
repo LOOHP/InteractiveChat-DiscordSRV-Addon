c34673993864d721d59a119db0b73596e2f64c46
MrCuttlefish_exe <45689155+Mrcuttlefish@users.noreply.github.com>
2026-05-10 15:43:02 -0400

    Added a reload to grab the CE resourcepack

--- common/src/main/java/com/loohp/interactivechatdiscordsrvaddon/InteractiveChatDiscordSrvAddon.java
+++ common/src/main/java/com/loohp/interactivechatdiscordsrvaddon/InteractiveChatDiscordSrvAddon.java
@@ -42,6 +42,7 @@ import com.loohp.interactivechatdiscordsrvaddon.api.events.ResourceManagerInitia
 import com.loohp.interactivechatdiscordsrvaddon.debug.Debug;
 import com.loohp.interactivechatdiscordsrvaddon.graphics.ImageGeneration;
 import com.loohp.interactivechatdiscordsrvaddon.graphics.ImageUtils;
+import com.loohp.interactivechatdiscordsrvaddon.hooks.craftengine.CraftEngineHook;
 import com.loohp.interactivechatdiscordsrvaddon.hooks.imageframe.ImageFrameEvents;
 import com.loohp.interactivechatdiscordsrvaddon.listeners.DiscordCommandEvents;
 import com.loohp.interactivechatdiscordsrvaddon.listeners.DiscordInteractionEvents;
@@ -780,6 +781,13 @@ public class InteractiveChatDiscordSrvAddon extends JavaPlugin implements Listen
                     String resourceName = serverResourcePack.getName();
                     Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "[ICDiscordSrvAddon] Loading \"" + resourceName + "\" resources...");
                     sources.add(ResourcePackSource.ofCustom(resourceName, serverResourcePack, ResourcePackType.SERVER));
+                }
+                if (craftEngineHook) {
+                    File cePackFile = CraftEngineHook.getGeneratedResourcePackFile();
+                    if (cePackFile != null) {
+                        Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "[ICDiscordSrvAddon] Loading \"CraftEngine\" resources...");
+                        sources.add(ResourcePackSource.ofCustom("CraftEngine", cePackFile, ResourcePackType.SERVER));
+                    }
                 }
                 resourceManager.loadResources(sources, (source, info) -> {
                     String resourceName = source.getName();
